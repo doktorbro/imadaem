9ce75aba208c0976fba91304d3ebe66d16fafbdd
Anatol Broder <anatol@penibelst.de>
2014-02-23 21:05:19 +0100

    Add attribute selector test

--- test/hello.js
+++ test/hello.js
@@ -1,3 +1,5 @@
-test( "hello test", function() {
-ok( 1 == "1", "Passed!" );
+test('attribute selector', 1, function() {
+  $('#qunit-fixture').append('<img data-imadaem>');
+  equal($('img[data-imadaem]').imadaem().length, 1,
+    'Attribute selector is matched');
 });
