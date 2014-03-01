f4dc385f71ccb2cdb43bffdc44e2094dad26f2e3
Anatol Broder <anatol@penibelst.de>
2014-03-01 18:16:35 +0100

    Fix indentation according to Google Style

--- js/jquery.imadaem.js
+++ js/jquery.imadaem.js
@@ -5,128 +5,129 @@
 
   $.fn.imadaem = function(options) {
     var
-      $elements = this,
+        $elements = this,
 
-      settings = $.extend({
-        dataAttribute: 'imadaem',
-        timthumbPath: '/timthumb/timthumb.php',
-        verticalRhythm: null,
-        windowEvents: 'resize orientationchange'
-      }, options),
+        settings = $.extend({
+          dataAttribute: 'imadaem',
+          timthumbPath: '/timthumb/timthumb.php',
+          verticalRhythm: null,
+          windowEvents: 'resize orientationchange'
+        }, options),
 
-      getNativeLength = function(cssLength) {
-        var density = window.devicePixelRatio || 1;
-        return Math.round(cssLength * density);
-      },
+        getNativeLength = function(cssLength) {
+          var density = window.devicePixelRatio || 1;
+          return Math.round(cssLength * density);
+        },
 
-      lineHeight = function($element) {
-        var lh = parseFloat($element.css('line-height'));
-        return isNaN(lh) ? 0 : lh;
-      },
+        lineHeight = function($element) {
+          var lh = parseFloat($element.css('line-height'));
+          return isNaN(lh) ? 0 : lh;
+        },
 
-      adjustVerticalRhythm = function($element, height) {
-        if (settings.verticalRhythm === 'line-height') {
-          var lh, l;
-          lh = lineHeight($element);
-          if (lh) {
-            l = Math.max(1, Math.round(height / lh));
-            height = lh * l;
-          }
-        }
-        return height;
-      },
-
-      getData = function($element) {
-        var data = $element.data(settings.dataAttribute);
-
-        if ($.isPlainObject(data)) {
-          // ratio must be a number
-          data.ratio = parseFloat(data.ratio) || 0;
-          // ignore maxRatio if ratio is set
-          data.maxRatio = data.ratio ? 0 : parseFloat(data.maxRatio) || 0;
-          // gravity must be a combination of ['l', 'r', 't', 'b']
-          data.gravity = data.gravity ?
-            data.gravity.replace(/[^lrtb]/g, '').substr(0, 2) :
-            '';
-        } else {
-          data = {url: data};
-        }
-
-        return $.extend({
-          url: '',
-          gravity: '',
-          ratio: 0,
-          maxRatio: 0,
-          heightGuide: ''
-        }, data);
-      },
-
-      setSrc = function($element, newSrc) {
-        var
-          oldSrc = $element.attr('src'),
-          errors = 0;
-
-        $element
-          .on('error', function() {
-            // fall back to the previous src once
-            if (!errors) {
-              errors += 1;
-              $(this).attr('src', oldSrc);
+        adjustVerticalRhythm = function($element, height) {
+          if (settings.verticalRhythm === 'line-height') {
+            var lh, l;
+            lh = lineHeight($element);
+            if (lh) {
+              l = Math.max(1, Math.round(height / lh));
+              height = lh * l;
             }
-          })
-          .attr('src', newSrc);
-      },
+          }
+          return height;
+        },
 
-      scale = function() {
-        var
-          $this,
-          data,
-          timthumbParams,
-          height,
-          minHeight,
-          width;
+        getData = function($element) {
+          var data = $element.data(settings.dataAttribute);
 
-        $elements.each(function() {
-          $this = $(this);
-
-          data = getData($this);
-          if (!data.url) {
-            return;
+          if ($.isPlainObject(data)) {
+            // ratio must be a number
+            data.ratio = parseFloat(data.ratio) || 0;
+            // ignore maxRatio if ratio is set
+            data.maxRatio = data.ratio ? 0 : parseFloat(data.maxRatio) || 0;
+            // gravity must be a combination of ['l', 'r', 't', 'b']
+            data.gravity = data.gravity ?
+              data.gravity.replace(/[^lrtb]/g, '').substr(0, 2) :
+              '';
+          } else {
+            data = {url: data};
           }
 
-          width = $this.innerWidth();
-          height = $this.innerHeight();
+          return $.extend({
+            url: '',
+            gravity: '',
+            ratio: 0,
+            maxRatio: 0,
+            heightGuide: ''
+          }, data);
+        },
 
-          if (data.ratio) {
-            height = Math.round(width / data.ratio);
-          } else if ($(data.heightGuide).length) {
-            height = $(data.heightGuide).innerHeight();
-          }
+        setSrc = function($element, newSrc) {
+          var
+              oldSrc = $element.attr('src'),
+              errors = 0;
 
-          if (data.maxRatio) {
-            minHeight = Math.round(width / data.maxRatio);
-            height = Math.max(minHeight, height);
-          }
+          $element.
+              on('error', function() {
+                // fall back to the previous src once
+                if (!errors) {
+                  errors += 1;
+                  $(this).attr('src', oldSrc);
+                }
+              }).
+              attr('src', newSrc);
+        },
 
-          height = adjustVerticalRhythm($this, height);
+        scale = function() {
+          var
+              $this,
+              data,
+              timthumbParams,
+              height,
+              minHeight,
+              width;
 
-          // prevent blinking effects
-          $this.height(height);
+          $elements.each(function() {
+            $this = $(this);
 
-          timthumbParams = {
-            src: data.url || '',
-            a: data.gravity || '',
-            w: getNativeLength(width),
-            h: getNativeLength(height)
-          };
+            data = getData($this);
+            if (!data.url) {
+              return;
+            }
 
-          setSrc($this, settings.timthumbPath + '?' + $.param(timthumbParams));
-        });
-      };
+            width = $this.innerWidth();
+            height = $this.innerHeight();
 
-    $(window)
-      .one('load', scale)
-      .on(settings.windowEvents, scale);
+            if (data.ratio) {
+              height = Math.round(width / data.ratio);
+            } else if ($(data.heightGuide).length) {
+              height = $(data.heightGuide).innerHeight();
+            }
+
+            if (data.maxRatio) {
+              minHeight = Math.round(width / data.maxRatio);
+              height = Math.max(minHeight, height);
+            }
+
+            height = adjustVerticalRhythm($this, height);
+
+            // prevent blinking effects
+            $this.height(height);
+
+            timthumbParams = {
+              src: data.url || '',
+              a: data.gravity || '',
+              w: getNativeLength(width),
+              h: getNativeLength(height)
+            };
+
+            setSrc($this,
+                settings.timthumbPath + '?' + $.param(timthumbParams));
+          });
+        };
+
+    $(window).
+        one('load', scale).
+        on(settings.windowEvents, scale);
 
     return this;
   };
