724815929d573cceabfd2cbe117238be20043042
Anatol Broder <anatol@penibelst.de>
2014-03-02 12:11:07 +0100

    Fix attribute selector test

--- test/core.js
+++ test/core.js
@@ -1,9 +1,9 @@
-test('attribute selector', 1, function() {
-  $(document).ready(function() {
-    $('#qunit-fixture').append('<img data-imadaem>');
-    equal(
-        $('img[data-imadaem]').imadaem().length,
-        1,
-        'Attribute selector is matched');
-  });
+test('attribute selector', function() {
+  expect(1);
+
+  $('#qunit-fixture').append('<img data-imadaem>');
+  equal(
+      $('img[data-imadaem]').imadaem().length,
+      1,
+      'Attribute selector is matched');
 });
