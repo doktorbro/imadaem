03af9a654d2830f2720bdfa136fb9671954c8b65
Anatol Broder <anatol@penibelst.de>
2014-03-02 12:20:08 +0100

    Add callback tests

---
+++ test/options-url.js
@@ -0,0 +1,34 @@
+test('empty callback', function() {
+  expect(1);
+
+  $('#qunit-fixture').append('<img class="empty">');
+
+  var i = $('img.empty').imadaem({
+    url: $.noop()
+  });
+
+  $(window).trigger('resize');
+
+  equal(i.attr('src'), undefined, 'Url is undefined');
+});
+
+
+test('data url callback', function() {
+  expect(2);
+  var TINIEST_GIF = 'data:image/gif;base64,\
+    R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+
+  $('#qunit-fixture').append('<img class="tiny">');
+
+  var i = $('img.tiny').imadaem({
+    url: function() {
+      return TINIEST_GIF;
+    }
+  });
+
+  equal(i.attr('src'), undefined, 'Url is undefined');
+
+  $(window).trigger('resize');
+
+  equal(i.attr('src'), TINIEST_GIF, 'Url is data');
+});
